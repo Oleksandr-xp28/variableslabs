3991eff284f1d28efa9ac2e61ec301cb6d17f0c1
Oleksandr-xp28 <128472063+Oleksandr-xp28@users.noreply.github.com>
2023-03-21 17:21:19 +0200

    Update variableslabs.py
    Ex_5 task done

--- variableslabs.py
+++ variableslabs.py
@@ -1,1 +1,7 @@
+width = float(input("Enter the width of the rectangle: "))  
+height = float(input("Enter the height of the rectangle: ")) 
+
+area = width * height  
+
+print(f"The area of the rectangle with width {width} and height {height} is {area}")  
 
